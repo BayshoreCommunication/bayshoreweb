e36d14d0e1c61cb4880e0fbe8cceef3b8e5ad537
fuadbackend <fuadbackend@gmail.com>
2023-07-27 11:14:43 +0600

    service update

--- app/(pages)/our-services/[service]/page.tsx
+++ app/(pages)/our-services/[service]/page.tsx
@@ -1,32 +1,37 @@
 "use client";
 import { services } from "@/components/unique/services/Service";
 import SectionLayout from "@/components/universal/SectionLayout";
-// import { usePathname } from "next/navigation";
+import { StickyContainer, Sticky } from "react-sticky";
+import Parser from "html-react-parser";
+import Link from "next/link";
 
-const page = () => {
-  // const pathname = usePathname();
-  // console.log("test", pathname.split("/")[2]);
+const page = ({ params }: { params: { service: string } }) => {
+  const parameter = params.service;
+  const individualService = services.filter((elem)=> elem.url === parameter)
 
   return (
     <SectionLayout bg="">
-      <div className="grid grid-cols-[65%_30%] gap-8">
-        <div className="bg-[#121] h-[200vh]">scroll</div>
-
-        <div className="sticky top-[72px] h-[80vh]">
-          <ul className="flex flex-col bg-[#F4F4F4] gap-6 py-8">
-            {services.map((el: any, i: number) => (
-              <li
-                className="mx-[3rem] rounded-[5px] text-small py-2 px-8 bg-[#fff]"
-                key={i}
-              >
-                {el.title}
-              </li>
-            ))}
-          </ul>
-        </div>
+  <div className="grid grid-cols-[65%_30%] gap-8">
+    {individualService.map((elem, index)=>
+    <div className="h-[100%] service-style" key={index}>
+      <h1>{elem.title}</h1>
+      {Parser(elem.description)}
       </div>
-    </SectionLayout>
-    // </>
+    )}
+    <div className="sticky top-[72px] h-[100%]">
+      <ul className="flex flex-col bg-[#F4F4F4] gap-6 py-8">
+        {services.map((el: any, i: number) => (
+          <li
+            className="mx-[3rem] rounded-[5px] text-small py-2 px-8 bg-[#fff]"
+            key={i}
+          >
+            <Link href={`/our-services/${el.url}`}>{el.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+</SectionLayout>
   );
 };
 
