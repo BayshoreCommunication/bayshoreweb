df9aecb913c3e08df91e4a06773027804d19206e
fuadbackend <fuadbackend@gmail.com>
2023-08-20 19:00:17 +0600

    test

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -5,13 +5,14 @@ import Image from "next/image";
 import React from "react";
 import { Metadata } from "next";
 import SimpleMap from "@/components/unique/contact/Map";
+import Form from "@/components/unique/contact/Form";
 
 export const metadata: Metadata = {
   title: "Contact-Bayshore Communication",
   description:
     "Are you concerned about your business growth? Bayshore's here to help. Contact us today and get a free consultation. Let us show you how we can make a difference for your business.",
 };
-import { useEffect, useState } from "react";
+
 
 let address: {
   title: string;
@@ -83,62 +84,7 @@ const Page = () => {
 
 export default Page;
 
-const Form = () => {
-  const [email, setEmail] = useState("");
 
-  useEffect(() => {
-    // Perform localStorage action
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const emailValue: string | null = localStorage.getItem("email");
-    if (emailValue) {
-      setEmail(emailValue);
-    }
-  }, []);
-
-  return (
-    <form className="flex flex-col items-center mt-8 gap-8">
-      <input
-        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
-        id="name"
-        type="text"
-        placeholder="Enter your name"
-      />
-      <input
-        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
-        id="email"
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-      />
-      <input
-        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
-        id="contact"
-        type="tel"
-        placeholder="Enter your contact no."
-      />
-      <input
-        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
-        id="website"
-        type="url"
-        placeholder="Enter your website"
-      />
-      <input
-        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
-        id="subject"
-        type="text"
-        placeholder="Enter the subject"
-      />
-      <textarea
-        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-44 resize-none text-small !text-[#6C757D]"
-        id="description"
-        placeholder="Enter your description"
-      ></textarea>
-      <button className="btn mt-4 text-small" type="button">
-        Send Message
-      </button>
-    </form>
-  );
-};
 
 
 const Row = ({ title, icon, address, phone, email }: any) => {
---
+++ components/unique/contact/Form.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    // Perform localStorage action
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const emailValue: string | null = localStorage.getItem("email");
+    if (emailValue) {
+      setEmail(emailValue);
+    }
+  }, []);
+
+  return (
+    <form className="flex flex-col items-center mt-8 gap-8">
+      <input
+        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
+        id="name"
+        type="text"
+        placeholder="Enter your name"
+      />
+      <input
+        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+      />
+      <input
+        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
+        id="contact"
+        type="tel"
+        placeholder="Enter your contact no."
+      />
+      <input
+        className="appearance-none border rounded w-full py-4 px-4 text-gray-700 border-[#CED4DA] leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
+        id="website"
+        type="url"
+        placeholder="Enter your website"
+      />
+      <input
+        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-small !text-[#6C757D]"
+        id="subject"
+        type="text"
+        placeholder="Enter the subject"
+      />
+      <textarea
+        className="appearance-none border border-[#CED4DA] rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-44 resize-none text-small !text-[#6C757D]"
+        id="description"
+        placeholder="Enter your description"
+      ></textarea>
+      <button className="btn mt-4 text-small" type="button">
+        Send Message
+      </button>
+    </form>
+  );
+};
+
+export default Form;
