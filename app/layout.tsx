111f941a5cb1f633d9afb0e27abe4369e1cb420a
AbdullahEsha <shahariaresha@gmail.com>
2023-11-30 15:57:06 +0600

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,19 @@ import { ReactNode } from "react";
 import "./globals.css";
 import Facebook from "@/components/universal/MessengerChatBot";
 import Head from "@/components/Head";
+import Script from "next/script";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <Head />
       <body>
+        <Script id="txtesdfasdf" strategy="lazyOnload">
+          {`
+           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN7P643"
+           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        `}
+        </Script>
         {children}
         <Facebook />
       </body>
--- components/Head.tsx
+++ components/Head.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
 import React from "react";
+import Script from "next/script";
 
 const Head = () => {
   const pathname = usePathname();
@@ -9,6 +10,15 @@ const Head = () => {
 
   return (
     <head>
+      <Script id="txtesdfasdf" strategy="lazyOnload">
+        {`
+           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+           })(window,document,'script','dataLayer','GTM-NN7P643');
+        `}
+      </Script>
       <link rel="canonical" href={process.env.NEXT_PUBLIC_APP_URL + url} />
     </head>
   );
