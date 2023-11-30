8bcefc157f268aa4eb93eb8416752b66e3b97693
AbdullahEsha <shahariaresha@gmail.com>
2023-11-30 15:21:41 +0600

    update head

--- components/Head.tsx
+++ components/Head.tsx
@@ -5,9 +5,11 @@ import React from "react";
 const Head = () => {
   const pathname = usePathname();
 
+  const url = pathname === "/" ? "" : pathname;
+
   return (
     <head>
-      <link rel="canonical" href={process.env.NEXT_PUBLIC_APP_URL + pathname} />
+      <link rel="canonical" href={process.env.NEXT_PUBLIC_APP_URL + url} />
     </head>
   );
 };
