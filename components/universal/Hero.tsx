07b62473dc5d3fb9159c34028a61caea469e8e65
arsahak <arsahak99@gmail.com>
2024-04-17 10:19:03 +0600

    update button

--- components/universal/Hero.tsx
+++ components/universal/Hero.tsx
@@ -9,7 +9,7 @@ export const HeroWithImage = ({ heading, description, imgLink }: any) => {
   return (
     <SectionLayout bg="bg-[#F4F4F4]">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12  items-center">
-        <HeroLeft heading={heading} description={description} />
+        <HeroLeft heading={heading} description={description} text={""} />
         <div className="px-[2rem] sm:px-[6rem] md:px-0">
           <Image
             src={imgLink}
@@ -28,7 +28,7 @@ const Hero = ({ heading, description }: any) => {
   return (
     <SectionLayout bg="bg-[#F4F4F4]">
       <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center gap-16">
-        <HeroLeft heading={heading} description={description} />
+        <HeroLeft heading={heading} description={description} text={""} />
         <HeroRight />
       </div>
     </SectionLayout>
@@ -39,7 +39,7 @@ export const HeroIndividual: any = ({ heading, description, imgLink }: any) => {
   return (
     <SectionLayout bg="bg-[#F4F4F4]">
       <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center gap-16">
-        <HeroLeft heading={heading} description={description} />
+        <HeroLeft heading={heading} description={description} text={""} />
         <Image
           src={imgLink}
           alt="marketing"
