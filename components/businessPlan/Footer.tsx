a58de2976b2e0ac57d3d0f20a8d5b01d5c9a3067
sajolbd <shamim15-9760@diu.edu.bd>
2025-09-15 11:08:35 +0600

    CTA button added

--- components/businessPlan/Footer.tsx
+++ components/businessPlan/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
 
         {/* Logo */}
         <motion.div
-          className="flex justify-center mb-6"
+          className="flex justify-center py-16"
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.5, duration: 0.8 }}
