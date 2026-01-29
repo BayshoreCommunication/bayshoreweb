d8df5f3b90a78d3d74a3fa27f411cb92a1ce08d8
arsahak <arsahak99@gmail.com>
2026-01-29 14:05:24 +0600

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,40 @@
+import Head from "@/components/Head";
+import SmoothScrolling from "@/components/motion/SmoothScrolling";
+import Facebook from "@/components/universal/MessengerChatBot";
+import Script from "next/script";
 import { ReactNode } from "react";
 import "./globals.css";
-import Facebook from "@/components/universal/MessengerChatBot";
-import Head from "@/components/Head";
-import Script from "next/script";
-import SmoothScrolling from "@/components/motion/SmoothScrolling";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <Head />
       <body>
-        <Script id="txtesdfasdf" strategy="lazyOnload">
-          {`
-           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN7P643"
-           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        `}
-        </Script>
+        {/* Google Tag Manager - noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NN7P643"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager - main script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQG7GW2V');
+            `,
+          }}
+        />
+
         <SmoothScrolling>{children}</SmoothScrolling>
         <Facebook />
       </body>
