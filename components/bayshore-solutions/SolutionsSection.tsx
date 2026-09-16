1a60714e318f28b5b7da97cd87feea72efb78e00
sajolbd <shamim15-9760@diu.edu.bd>
2026-09-16 16:43:59 +0600

    Update solution card button text to Find Talent to match solutions page standard

--- components/bayshore-solutions/SolutionsSection.tsx
+++ components/bayshore-solutions/SolutionsSection.tsx
@@ -502,7 +502,7 @@ export const SolutionsSection: React.FC<SolutionsSectionProps> = ({
                       : "bg-[#07192C] text-white hover:bg-[#000e1e]"
                   }`}
                 >
-                  <span>Hire Now</span>
+                  <span>Find Talent</span>
                   <FiArrowRight size={16} className="transition-transform duration-300 group-hover:translate-x-1" />
                 </motion.button>
               </motion.div>
